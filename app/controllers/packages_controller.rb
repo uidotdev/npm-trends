b257eb93a6cf97481131d1fc3ecd64a565a2f31f
John Potter <johnpotter1440@gmail.com>
2019-02-06 14:32:20 -0800

    respond to urls that end in .js as html

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,10 +2,10 @@ class PackagesController < ApplicationController
 
 	def index
 		@title = params[:id].present? ? params[:id].gsub('-', ' ') + " | npm trends" : "npm trends: Compare NPM package downloads"
-		@description = params[:id].present? ? 
+		@description = params[:id].present? ?
 			"Compare npm package download statistics over time: " + params[:id].gsub('-', ' ') :
 			"Which NPM package should you use? Compare NPM package download stats over time. Spot trends, pick the winner!"
-			
+		render 'index.html.erb', layout: 'application.html.erb', content_type: 'text/html'
 	end
 
 end
