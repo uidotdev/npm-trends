1649451d54bf40b873852724d7c4de22556a6ae2
John Potter <johnpotter1440@gmail.com>
2019-05-01 08:20:27 -0700

    fix canonical for empty query

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -5,7 +5,7 @@ class PackagesController < ApplicationController
 		@description = params[:id].present? ?
 			"Compare npm package download statistics over time: " + params[:id].gsub('-', ' ') :
 			"Which NPM package should you use? Compare NPM package download stats over time. Spot trends, pick the winner!"
-		@canonical = SearchQuery.canonical_url(params[:id])
+		@canonical = params[:id] ? SearchQuery.canonical_url(params[:id]) : nil
 		render 'index.html.erb', layout: 'application.html.erb', content_type: 'text/html'
 	end
 
