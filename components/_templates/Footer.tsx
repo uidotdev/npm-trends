594e08ceebe9c3b13e05a65d91caef7b1bf8db16
Benjamin Adam <benjamin.mark.adam@gmail.com>
2022-06-27 09:29:02 -0700

    Update footer link

--- components/_templates/Footer.tsx
+++ components/_templates/Footer.tsx
@@ -4,7 +4,11 @@ const Footer = () => (
       <div className="footer-content">
         <ul id="footer_menu" className="list-unstyled">
           <li className="inline">
-            Made by{' '}
+            From{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://ui.dev">
+              uidotdev
+            </a>
+           {' '} - created by{' '}
             <a target="_blank" rel="noopener noreferrer" href="http://twitter.com/johnmpotter">
               John Potter
             </a>
