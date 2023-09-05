c47ddc5bef82ef7c7038c19f9b33a9266ee3fc91
Benjamin Adam <benjamin.mark.adam@gmail.com>
2023-09-05 08:11:12 -0700

    Fix countdown timer

--- components/_components/Banners/ReactGGPromoBanner.tsx
+++ components/_components/Banners/ReactGGPromoBanner.tsx
@@ -58,7 +58,7 @@ function timeUntilTargetPST(targetYear, targetMonth, targetDay) {
     hours: String(hours).padStart(2, '0'),
     minutes: String(minutes).padStart(2, '0'),
     seconds: String(seconds).padStart(2, '0'),
-    hasExpired: delta <= 0,
+    hasExpired: currentUTC > targetUTC,
   };
 }
 
